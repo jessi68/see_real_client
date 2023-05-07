f2346a215d4cdde93053fc90451a120230611385
jessi68 <47855517+jessi68@users.noreply.github.com>
2023-05-08 03:25:17 +0900

    feat: putting dream ui

--- Assets/Samples/Mixed Reality Toolkit Examples/2.8.3/Demos - HandTracking/Scripts/Interaction/TemporaryUI.cs
+++ Assets/Samples/Mixed Reality Toolkit Examples/2.8.3/Demos - HandTracking/Scripts/Interaction/TemporaryUI.cs
@@ -95,7 +95,6 @@ public class TemporaryUI : MonoBehaviour
                 TextWriterSingleton.addValue(answerNumbers[i].ToString());
             }
 
-            InteractionManager.MoveOnNextInteraction();
 
         }
 
