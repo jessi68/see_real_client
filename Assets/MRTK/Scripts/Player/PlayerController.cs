0d6538210b4cbc9baf073444a96189201dced22b
jessi68 <47855517+jessi68@users.noreply.github.com>
2023-05-12 11:22:47 +0900

    f

--- Assets/MRTK/Scripts/Player/PlayerController.cs
+++ Assets/MRTK/Scripts/Player/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     bool isUpdated = false;
+    float elaspedTime = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +15,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!isUpdated)
-        {
-            
-            transform.position = new Vector3(-3.49f, -3.35f, 0f);
+        elaspedTime += Time.deltaTime;
+        Debug.Log(elaspedTime);
+
+        if(elaspedTime > 5 && isUpdated == false) { 
+            transform.position = new Vector3(-3.980347f, -5.62f, -2.959058f);
             transform.rotation = Quaternion.Euler(0f, 183.06f, 0f);
+            Debug.Log("eeeeee");
             isUpdated = true; 
         }
+
     
     }
 }
